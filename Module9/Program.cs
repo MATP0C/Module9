3914abb6a6d48e0b42e52e8f1a4a3427c120d151
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-19 10:27:12 +0300

    9.3.3

--- Module9/Program.cs
+++ Module9/Program.cs
@@ -9,17 +9,24 @@ namespace Module9
     class Program
     {
         delegate int CalculateDelegate(int a, int b);
+        delegate int SumDelegate(int c, int d);
         static void Main(string[] args)
         {
 
             CalculateDelegate calcDelegate = Calculate;
-            int result = calcDelegate.Invoke(100, 30);
+            int result1 = calcDelegate.Invoke(100, 30);
+            SumDelegate sumDelegate = Sum;
+            int result2 = sumDelegate(200, 60);
 
-            Console.WriteLine(result);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
             Console.Read();
 
         }
-
+        static int Sum(int c, int d)
+        {
+            return c + d;
+        }
         static int Calculate(int a, int b)
         {
             return a - b;
