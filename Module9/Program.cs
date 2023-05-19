c9ccbbf29c4ed37bb0e4d768b4b1364abb10fb5b
bogdan <bogdan.kuznetsov2021@yandex.ru>
2023-05-19 11:11:04 +0300

    9.3.12

--- Module9/Program.cs
+++ Module9/Program.cs
@@ -8,40 +8,15 @@ namespace Module9
 {
     class Program
     {
-        delegate void ShowMessageDelegate();
-        delegate int SumDelegate(int a, int b, int c);
-        delegate bool CheckLengthDelegate(string row);
-
+        delegate void ShowMessageDelegate(string _message);
         static void Main(string[] args)
         {
-            ShowMessageDelegate showMessageDelegate = ShowMessage;
-            showMessageDelegate.Invoke();
-
-            SumDelegate sumDelegate = Sum;
-            int result = sumDelegate.Invoke(1, 30, 120);
-            Console.WriteLine(result);
-
-            CheckLengthDelegate checkLengthDelegate = CheckLength;
-            bool status = checkLengthDelegate.Invoke("skill_factory");
-            Console.WriteLine(status);
-
-            Console.ReadLine();
-        }
-
-        static void ShowMessage()
-        {
-            Console.WriteLine("Hello World!");
-        }
-
-        static int Sum(int a, int b, int c)
-        {
-            return a + b + c;
-        }
-
-        static bool CheckLength(string _row)
-        {
-            if (_row.Length > 3) return true;
-            return false;
+            ShowMessageDelegate showMessageDelegate = delegate (string _message)
+            {
+                Console.WriteLine(_message);
+            };
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
         }
     }
 }
